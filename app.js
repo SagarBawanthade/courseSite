665af76828912d7d2c1e831f63307ff3fc8be3e7
Sagar Bawanthade <137410094+SagarBawanthade@users.noreply.github.com>
2024-07-23 14:11:38 +0530

    Update app.js

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use(express.urlencoded({
 }));
 app.use(cookieParser());
 app.use(cors({
-    origin: "https://localhost:3000",
+    origin: "http://localhost:3000",
     credentials: true,
 }));
 
