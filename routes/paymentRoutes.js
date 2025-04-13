22c218afb70de14406dc0e1d7ab7f073fb4aaa26
Sagar Bawanthade <sagar.bawanthade2004@gmail.com>
2025-04-13 12:28:17 +0530

    fixed color filter

--- routes/paymentRoutes.js
+++ routes/paymentRoutes.js
@@ -8,13 +8,13 @@ const router = express.Router();
 router.route("/subscribe").get( buySubscription);
 
 //verify payment ans save refernece in DB
-router.route("/paymentverification").post(isAuthenticated, paymentVerification);
+router.route("/paymentverification").post( paymentVerification);
 
 //get razorpay key
 router.route("/razorpaykey").get(getRazorPaykey);
 
 //cancel subscription
-router.route("/subscribe/cancel").delete(isAuthenticated, cancelSubscription);
+router.route("/subscribe/cancel").delete( cancelSubscription);
 
 
 export default router;  
